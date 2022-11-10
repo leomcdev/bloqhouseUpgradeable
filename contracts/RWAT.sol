85159b402d7218325337ef4d2df2e2d67a34cc9c
leo <leobmcneil@gmail.com>
2022-11-10 15:15:53 +0100

    updated code

--- contracts/RWAT.sol
+++ contracts/RWAT.sol
@@ -2,7 +2,13 @@
 
 pragma solidity ^0.8.4;
 
-import "./Asset.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+
+import "../interfaces/ICNR.sol";
 
 /**
  * @title Real world asset tokenization contract to fractionalize real-estate into shares.
@@ -12,8 +18,20 @@ import "./Asset.sol";
  * state variables in this contract. All new inherited contracts must be appeneded
  * to the currently inherited contracts.
  */
-contract RWAT is Asset {
+
+contract RWAT is
+    Initializable,
+    ERC721Upgradeable,
+    AccessControlUpgradeable,
+    PausableUpgradeable
+{
     bytes32 public constant ADMIN = keccak256("ADMIN");
+
+    /**
+     * @notice Called first in the initialize (RWAT) contract upon deployment. Functions with
+     * state variables that are not stated as CONSTANTS are required to be declared with
+     * the onlyInitalizing statement, to not interrupt the initialize call in the RWAT contract.
+     */
 
     /**
      * @notice Called once to configure the contract after the initial deployment.
@@ -22,58 +40,105 @@ contract RWAT is Asset {
     function initialize(
         address _default_admin,
         ICNR _CNR,
-        string memory name_,
-        string memory symbol_
+        string memory _name,
+        string memory _symbol
     ) external initializer {
         __AccessControl_init();
         _setupRole(DEFAULT_ADMIN_ROLE, _default_admin);
 
-        __ERC721_init(name_, symbol_);
+        __ERC721_init(_name, _symbol);
 
         initializeCNR(_CNR);
-
         __Pausable_init();
     }
 
-    function setWhitelisted(address[] calldata _users, bool _isWhitelisted)
-        external
-        onlyRole(ADMIN)
-    {
-        _setWhitelisted(_users, _isWhitelisted);
+    function initializeCNR(ICNR _CNR) internal onlyInitializing {
+        CNR = _CNR;
     }
 
+    // ------------ Events
+    event UnitsClaimed(address claimant, uint256[] tokenIds);
+    event EarningsClaimed(
+        address claimant,
+        uint256 assetId,
+        uint256[] tokenIds
+    );
+
+    ICNR private CNR;
+    mapping(address => bool) public isWhitelisted;
+
+    bool pausedTransfers;
+    mapping(uint256 => bool) public assetPaused;
+
+    mapping(uint256 => uint256) public nextId;
+    mapping(uint256 => uint256) public lastId;
+
+    mapping(uint256 => IERC20Upgradeable) public assetEarningsToken;
+    mapping(uint256 => uint256) public totalShareEarnings;
+    mapping(uint256 => uint256) public claimedEarnings;
+
+    mapping(uint256 => uint256) public assetIdToAssetCap;
+
+    address serverPubKey;
+    string name_;
+    string symbol_;
+
     /**
-     * @notice Creates asset with with ID and tokenCap.
-     * @dev Adds 9 zeros to the ID.
+     * @notice Creates the asset with a token cap.
+     * @dev 9 zeros are added.
      */
     function createAsset(
         uint256 _assetId,
         uint256 _tokenCap,
-        IERC20Upgradeable _revToken
+        IERC20Upgradeable _EarningsToken
     ) external onlyRole(ADMIN) {
-        _createAsset(_assetId, _tokenCap, _revToken);
+        require(nextId[_assetId] == 0, "Asset already exists");
+        require(
+            nextId[_assetId] < _tokenCap,
+            "Asset ID can't be higher than max token cap"
+        );
+        assetEarningsToken[_assetId] = _EarningsToken;
+        nextId[_assetId] = _assetId * 1_000_000_000;
+        lastId[_assetId] = _assetId * 1_000_000_000 + _tokenCap;
+
+        assetIdToAssetCap[_assetId] = _tokenCap;
     }
 
     /**
-     * @notice Updates the current asset cap.
-     * @dev 9 zeros are added.
+     * @notice Updates asset cap.
      */
     function updateAssetCap(uint256 _assetId, uint256 _tokenCap)
         external
         onlyRole(ADMIN)
     {
-        _updateAssetTokenCap(_assetId, _tokenCap);
+        require(
+            nextId[_assetId] <= _assetId * 1_000_000_000 + _tokenCap,
+            "Asset cap can not be lower than minted amount"
+        );
+        lastId[_assetId] = _assetId * 1_000_000_000 + _tokenCap;
+
+        assetIdToAssetCap[_assetId] = _tokenCap;
     }
 
     /**
      * @notice Mints assets with respective ID as long as the max amount
      * of minted assets has not been exceeded.
      */
+
     function mintAsset(uint256 _assetId, uint256 _amount)
         external
         onlyRole(ADMIN)
     {
-        _mintAsset(_assetId, _amount, address(this));
+        require(
+            (nextId[_assetId] + _amount) <= lastId[_assetId],
+            "Amount exceeds max"
+        );
+        uint256 mints = nextId[_assetId] + _amount;
+
+        for (uint256 i = nextId[_assetId]; i < mints; i++) {
+            _mint(address(this), i);
+        }
+        nextId[_assetId] = mints;
     }
 
     /**
@@ -99,7 +164,7 @@ contract RWAT is Asset {
         );
 
         uint256 assetId = _getTokenAsset(_tokenIds[0]);
-        uint256 totalClaim = totalShareRev[assetId];
+        uint256 totalClaim = totalShareEarnings[assetId];
         _setClaimed(assetId, _tokenIds, totalClaim);
         _claimUnits(address(this), msg.sender, _tokenIds);
         emit UnitsClaimed(msg.sender, _tokenIds);
@@ -117,51 +182,127 @@ contract RWAT is Asset {
     }
 
     /**
-     * @notice Calculates and adds the revenue to a asset.
+     * @notice Whitelists multiple users to be available for shares.
+     * @dev Also deWhitelists users by setting to false.
      */
-    function addRevenue(
+    function setWhitelisted(address[] calldata _users, bool _whitelisted)
+        external
+        onlyRole(ADMIN)
+    {
+        uint256 length = _users.length;
+        for (uint256 i = 0; i < length; i++) {
+            isWhitelisted[_users[i]] = _whitelisted;
+        }
+    }
+
+    /**
+     * @notice User claim units.
+     */
+    function _claimUnits(
+        address _from,
+        address _to,
+        uint256[] calldata _tokenIds
+    ) internal {
+        uint256 length = _tokenIds.length;
+        for (uint256 i = 0; i < length; i++) {
+            _transfer(_from, _to, _tokenIds[i]);
+        }
+    }
+
+    /**
+     * @notice Set units to claimed.
+     */
+    function _setClaimed(
         uint256 _assetId,
-        uint256 _totalRev,
+        uint256[] calldata _tokenIds,
+        uint256 _amount
+    ) internal {
+        uint256 length = _tokenIds.length;
+        for (uint256 i = 0; i < length; i++) {
+            require(
+                _getTokenAsset(_tokenIds[i]) == _assetId,
+                "Invalid token for asset"
+            );
+            claimedEarnings[_tokenIds[i]] = _amount;
+        }
+    }
+
+    /**
+     * @notice Calculates and adds the Earningsenue to a asset.
+     */
+    function addEarnings(
+        uint256 _assetId,
+        uint256 _totalEarnings,
         uint256 _unitsCount,
         uint256 _amountPerShare
     ) external onlyRole(ADMIN) {
         require(
-            _totalRev / _unitsCount == _amountPerShare,
+            _totalEarnings / _unitsCount == _amountPerShare,
             "Invalid input data"
         );
-        _addRevenue(msg.sender, _assetId, _totalRev, _amountPerShare);
+        _addEarnings(msg.sender, _assetId, _totalEarnings, _amountPerShare);
     }
 
     /**
-     * @notice User can claim its total revenue
+     * @dev Add Earningsenue into contract to later be added to the respecive assets.
      */
-    function claimRevenue(uint256 _assetId, uint256[] calldata _tokenIds)
-        external
-        whenNotPaused
-    {
-        _claimRevenue(msg.sender, _assetId, _tokenIds);
-        emit RevenueClaimed(msg.sender, _assetId, _tokenIds);
-    }
-
-    function updateServer(address _serverPubKey) external onlyRole(ADMIN) {
-        _updateServer(_serverPubKey);
+    function _addEarnings(
+        address _from,
+        uint256 _assetId,
+        uint256 _totalEarnings,
+        uint256 _amountPerToken
+    ) internal {
+        totalShareEarnings[_assetId] += _amountPerToken;
+        assetEarningsToken[_assetId].transferFrom(
+            _from,
+            address(this),
+            _totalEarnings
+        );
     }
 
     /**
-     * @notice Set transfers paused for the whole contract
+     * @notice Calculates the Earningsenue that each investor can claim.
+     * @dev Adds the new value into claimed Earnings and transfers the Earningsenue to the owner.
      */
+    function claimEarnings(
+        address _owner,
+        uint256 _assetId,
+        uint256[] calldata _tokenIds
+    ) external whenNotPaused {
+        require(isWhitelisted[_owner], "Owner is not whitelisted");
+        uint256 totalToGet;
+        uint256 length = _tokenIds.length;
+        uint256 totalAssetEarnings = totalShareEarnings[_assetId];
+        for (uint256 i = 0; i < length; i++) {
+            require(ownerOf(_tokenIds[i]) == _owner, "Invalid token owner");
+            require(
+                _getTokenAsset(_tokenIds[i]) == _assetId,
+                "Invalid token for asset"
+            );
+            totalToGet += totalAssetEarnings - claimedEarnings[_tokenIds[i]];
+            claimedEarnings[_tokenIds[i]] = totalAssetEarnings;
+        }
+        assetEarningsToken[_assetId].transferFrom(
+            address(this),
+            _owner,
+            totalToGet
+        );
+        emit EarningsClaimed(msg.sender, _assetId, _tokenIds);
+    }
+
     function setTransfersPaused(bool _paused) external onlyRole(ADMIN) {
-        _setTransfersPaused(_paused);
+        pausedTransfers = _paused;
     }
 
-    /**
-     * @notice Set transfers paused for a asset
-     */
     function setAssetTransfersPaused(uint256 _assetId, bool _paused)
         external
         onlyRole(ADMIN)
     {
-        _setAssetTransfersPaused(_assetId, _paused);
+        assetPaused[_assetId] = _paused;
+    }
+
+    function updateServer(address _serverPubKey) external onlyRole(ADMIN) {
+        serverPubKey = _serverPubKey;
     }
 
     function pause() external onlyRole(ADMIN) {
@@ -173,10 +314,30 @@ contract RWAT is Asset {
     }
 
     /**
+     * @notice Get current asset cap
+     */
+    function getAssetCap(uint256 _assetId) public view returns (uint256) {
+        // require exists
+        return assetIdToAssetCap[_assetId];
+    }
+
+    /**
+     * @notice Get total minted assets in circulation
+     */
+    function getTotalMinted(uint256 _assetId) public view returns (uint256) {
+        // require exists
+        return nextId[_assetId] - 1_000_000_000;
+    }
+
+    function _getTokenAsset(uint256 _tokenId) public pure returns (uint256) {
+        return _tokenId / 1_000_000_000;
+    }
+
+    /**
      * @notice Set and update name and symbol after deployment!
      */
     function setNameAndSymbol(string memory _name, string memory _symbol)
-        public
+        external
         onlyRole(ADMIN)
     {
         name_ = _name;
@@ -189,6 +350,49 @@ contract RWAT is Asset {
 
     function symbol() public view override returns (string memory) {
         return symbol_;
+    }
+
+    /**
+     * @notice Overrides the _beforeTokenTransfer in the ERC721Upgradeable contract
+     * @dev Checks state and that users are whitelisted.
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        super._beforeTokenTransfer(from, to, tokenId);
+        if (!(from == address(0) || from == address(this))) {
+            require(!pausedTransfers, "Transfers are currently paused");
+            require(!assetPaused[_getTokenAsset(tokenId)], "Asset is paused");
+            require(
+                isWhitelisted[from] && isWhitelisted[to],
+                "Invalid token transfer"
+            );
+        }
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        return ICNR(CNR).getNFTURI(address(this), _tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721Upgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
     uint256[1000] private __gap;
--- test/upgradeabletest.js
+++ test/upgradeabletest.js
@@ -5,14 +5,12 @@ const { ethers, upgrades } = require("hardhat");
 CNR = "0x0cadb0d9e410072325d2acc00aab99eb795a8c86";
 
 describe("Whitelist", function () {
-  let owner, provider, investor, asset, testToken;
+  let owner, provider, investor, testToken;
 
   beforeEach(async function () {
     [owner, provider, investor] = await ethers.getSigners();
     // CNR = await help.setCNR();
     testToken = await help.setTestToken();
-
-    // asset = await help.setAsset();
 
     const Rwat = await ethers.getContractFactory("RWAT");
     const rwat = await upgrades.deployProxy(
@@ -42,11 +40,24 @@ describe("Whitelist", function () {
     await rwat.grantRole(ADMIN, owner.address);
     await rwat.grantRole(ADMIN, provider.address);
 
-    await rwat.createAsset(1, 100, testToken.address);
+    await rwat.createAsset(1, 300, testToken.address);
     await rwat.mintAsset(1, 100);
+    console.log("total assets in circulation", await rwat.getTotalMinted(1));
     await rwat.setWhitelisted([investor.address], true);
     console.log(investor.address);
     let units = [1000000000, 1000000001, 1000000002];
+
+    console.log("current asset cap", await rwat.getAssetCap(1));
+
+    await rwat.updateAssetCap(1, 1000);
+    console.log("asset cap after update", await rwat.getAssetCap(1));
+
+    await rwat.mintAsset(1, 150);
+
+    console.log(
+      "total assets in circulation after creating more",
+      await rwat.getTotalMinted(1)
+    );
 
     let obj = ethers.utils.defaultAbiCoder.encode(
       ["address", "address", "uint[]"],
@@ -59,7 +70,7 @@ describe("Whitelist", function () {
     await rwat.connect(investor).claimUnits(units, prefix, v, r, s);
 
     expect(await rwat.ownerOf(1000000002)).to.be.equal(investor.address);
-    console.log(await rwat.balanceOf(investor.address));
+    // console.log(await rwat.balanceOf(investor.address));
 
     console.log(
       "hash admin",
